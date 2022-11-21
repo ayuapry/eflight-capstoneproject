70fd498d8be94269cd5d1f0a6706ba6e2e190490
Ayuapry <ayuapriyanti0304@gmail.com>
2022-11-21 23:59:38 +0700

    initial navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Homepage } from "./pages/Homepage";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        <h1>This is navbar</h1>
+    </div>
+  )
+}
