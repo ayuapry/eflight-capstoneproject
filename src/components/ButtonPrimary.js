43089677c41a7d92550c66077d9508b2759bf2c3
Stdivaazzahra <divaazzahra623@gmail.com>
2022-11-26 06:29:40 +0800

    Add Card

--- src/components/ButtonPrimary.js
+++ src/components/ButtonPrimary.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import {Button} from 'antd';
-import {PlayCircleOutlined} from '@ant-design/icons'
+import React from "react";
+import { Button } from "antd";
+import { PlayCircleOutlined } from "@ant-design/icons";
 
 const ButtonPrimary = (props) => {
-    return (
-        // <Button type="primary" shape="round" size='large'
-        // 	style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', backgroundColor: "#FFE69A", color: 'black', fontWeight: 'bold'}}
-        //     onClick={props.click}
-        //     htmlType={props.type}
-		// > {props.title} </Button>
-        <button type="submit" className="w-full text-slate-700 font-bold bg-[#FFD24C] hover:bg-[#FFE69A] focus:outline-none focus:ring-2 focus:ring-[#FFE69A] rounded-full text-sm px-5 py-2.5 text-center"
-        onClick={props.click}>
-        {props.title}</button>
-    );
+  return (
+    // <Button type="primary" shape="round" size='large'
+    // 	style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', backgroundColor: "#FFE69A", color: 'black', fontWeight: 'bold'}}
+    //     onClick={props.click}
+    //     htmlType={props.type}
+    // > {props.title} </Button>
+    <button
+      type="submit"
+      className="w-full text-slate-700 text-[0.9rem] font-bold bg-[#FFD24C] hover:bg-[#FFE69A] focus:outline-none focus:ring-2 focus:ring-[#FFE69A] rounded-xl text-sm px-5 py-2.5 text-center"
+      onClick={props.click}
+    >
+      {props.title}
+    </button>
+  );
 };
 
 export default ButtonPrimary;
--- src/index.js
+++ src/index.js
@@ -2,31 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProfilePage } from "./pages/ProfilePage";
 import { NotificationPage } from "./pages/NotificationPage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-<<<<<<< HEAD
 import Detail from "./pages/Detail";
-=======
 import { HistoryPage } from "./pages/HistoryPage";
->>>>>>> 7ce9c75308aa7a9035818532c4ae20853c9059b4
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/notification" element={<NotificationPage />} />
-        <Route path="/Register" element={<Register/>} />
-        <Route path="/Login" element={<Login/>} />
-<<<<<<< HEAD
-        <Route path="/Detail" element={<Detail/>} />
-=======
-        <Route path="/history" element={<HistoryPage/>} />
->>>>>>> 7ce9c75308aa7a9035818532c4ae20853c9059b4
-      </Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/notification" element={<NotificationPage />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Detail" element={<Detail />} />
+      <Route path="/history" element={<HistoryPage />} />
+    </Routes>
   </BrowserRouter>
 );
