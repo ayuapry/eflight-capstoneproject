7aa918e0866c3324543b3b22370b7a561ec13bdb
IRVAN RIZKY A <IrvanRizkyAriansyah>
2022-12-06 20:42:05 +0700

    detail

--- src/components/ButtonBorder.js
+++ src/components/ButtonBorder.js
@@ -8,7 +8,7 @@ const ButtonBorder = (props) => {
         //     onClick={props.click}
 		// > {props.title} {props.icon}</Button> 
         <button type="button" 
-        className="w-full font-bold py-2.5 px-5 text-sm hover:border-slate-700 text-slate-700 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+        className=" flex w-full font-bold py-2.5 px-5 text-sm hover:border-slate-700 text-slate-700 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
             {props.title} {props.icon}</button>
     );
 };
