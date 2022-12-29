66770394a2aed70e48c495d71a269b29668778a9
IrvanRizkyAriansyah <irvanrizky58@gmail.com>
2022-12-29 17:54:36 +0700

    download boarding pass

--- src/pages/CheckInPage.jsx
+++ src/pages/CheckInPage.jsx
@@ -1,6 +1,6 @@
-import { Form, Input } from "antd";
+import { Form, Input, Modal } from "antd";
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import login from "../assets/login.png";
@@ -9,26 +9,83 @@ import Footer from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 import ScrollToTop from "../components/ScrollToTop";
 import { SecondFooter } from "../components/SecondFooter";
-import { getBoardingPass, getCheckin, getHistory } from "../redux/feature/historySlice";
+import {
+  getBoardingPass,
+  getCheckin,
+  getHistory,
+} from "../redux/feature/historySlice";
+import checkinSuccess from "../assets/checkinSuccess.svg";
+import checkinFail from "../assets/checkinFail.svg";
 
 export const CheckInPage = () => {
   const { checkin, history } = useSelector((state) => state.history);
   const dispatch = useDispatch();
   const [form] = Form.useForm();
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [data, setData] = useState();
+  const checkinId = localStorage.getItem("checkinId");
 
+  const responModal = (props) => {
+    return (
+      <Modal
+        title=""
+        open={true}
+        footer={null}
+        centered={true}
+        closable={false}
+      >
+        <div className="flex items-center">
+          <p className="text-lg font-semibold m-0 items-center mx-auto">
+            {props.title}
+          </p>
+        </div>
 
-  const onFinish = (values) => {
-    // dispatch(getBoardingPass(values))
-    dispatch(getCheckin(values))
-    navigate("/history");
+        <img src={props.resImg} alt={props.type} className="h-72 mx-auto" />
+        <p>{props.message}</p>
+        <div className="mr-0 ml-auto">
+          <ButtonPrimary title={props.resTitle} click={props.resClick} />
+        </div>
+      </Modal>
+    );
   };
 
+  const onFinish = (values) => {
+    dispatch(getCheckin(values));
+    setData(values);
+  };
+
+  const downloadBoardPass = () => {
+    dispatch(getBoardingPass(data));
+    localStorage.removeItem("checkinId");
+    setIsModalOpen(false);
+    // navigate("/history");
+  };
+
+  const closeModal = () => {
+    window.location.reload(1);
+  };
 
   return (
     <div className="h-full md:min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
       <ScrollToTop />
       <Navbar />
+      {checkinId
+        ? responModal({
+            title: "Check In Success",
+            resImg: checkinSuccess,
+            resClick: downloadBoardPass,
+            resTitle: "Download Boarding Pass",
+          })
+        : checkin[0]
+        ? responModal({
+            title: "Check In Failed",
+            resImg: checkinFail,
+            resClick: closeModal,
+            resTitle: "Cancel",
+            message: checkin,
+          })
+        : ""}
       <div className="max-w-[1240px] mx-auto h-full md:min-h-[90vh]">
         <div className="md:pt-40 pt-20">
           <div className="bg-white md:mx-14 rounded-md shadow-md md:py-20 py-5 mx-2 px-5 md:h-[400px]">
