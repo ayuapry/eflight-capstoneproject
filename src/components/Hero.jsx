96f668a5fab9ee6e17f16da4713f145c99d3e9bd
IrvanRizkyAriansyah <irvanrizky58@gmail.com>
2022-12-30 08:27:39 +0700

    repair missing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,57 +1,82 @@
 // import Swiper core and required modules
-import React, { useEffect } from 'react';
-import {  Pagination, A11y, Autoplay } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useEffect } from "react";
+import { Pagination, A11y, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Divider, Form, Radio, Skeleton, Space, Switch } from "antd";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { useDispatch, useSelector } from 'react-redux';
-import { getHero } from '../redux/feature/homeSlice';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { useDispatch, useSelector } from "react-redux";
+import { getHero } from "../redux/feature/homeSlice";
 
 export const Hero = () => {
-    const {hero, loading} = useSelector((state) => state.homepage);
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-      dispatch(getHero())
-    },[dispatch]);  
-  
-    if(loading){
-      return <h2>Loading</h2>
-    }
+  const { hero, loading } = useSelector((state) => state.homepage);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getHero());
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div className="pt-[65px] md:pb-0 md:bg-blue-600 md:h-96 mb-0">
+        <div className="w-fit mx-auto">
+          <Skeleton.Button
+            active={true}
+            shape={"default"}
+            style={{
+              height: "23rem",
+              width: "48rem",
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
   return (
-    <div id='hero' className='pt-[65px] pb-10 md:pb-0 md:bg-blue-600 md:h-fit w-full'>
-        <Swiper
-          // install Swiper modules
-          modules={[Pagination, A11y, Autoplay]}
-          autoplay={{
-            delay: 10000,
-            disableOnInteraction: false,
+    <div
+      id="hero"
+      className="pt-[65px] pb-10 md:pb-0 md:bg-blue-600 md:h-fit w-full"
+    >
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination, A11y, Autoplay]}
+        autoplay={{
+          delay: 10000,
+          disableOnInteraction: false,
+        }}
+        spaceBetween={0}
+        slidesPerView={1}
+        loop={true}
+        pagination={{ clickable: true }}
+      >
+        {hero &&
+          hero.map((hero, i) => {
+            return (
+              <SwiperSlide key={i}>
+                <img
+                  className="md:max-w-3xl mx-auto"
+                  src={hero?.imageURL}
+                  alt="Banner1"
+                />
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+
+      {/* <div className="w-fit mx-auto">
+        <Skeleton.Button
+          active={true}
+          shape={"default"}
+          style={{
+            height: "23rem",
+            width: "48rem",
           }}
-          spaceBetween={0}
-          slidesPerView={1}
-          loop={true}
-          pagination={{ clickable: true }}
-        >
-            <SwiperSlide>
-                <img 
-                  className='md:max-w-3xl mx-auto'
-                  src={hero[0]?.imageURL} alt="Banner1" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img 
-                  className='md:max-w-3xl mx-auto' 
-                  src={hero[1]?.imageURL} alt="Banner2" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img 
-                  className='md:max-w-3xl mx-auto' 
-                  src={hero[2]?.imageURL} alt="Banner3" />
-            </SwiperSlide>
-        </Swiper>
+        />
+      </div> */}
     </div>
-  )
-}
+  );
+};
--- src/utils/Missing.js
+++ src/utils/Missing.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import ButtonPrimary from '../components/ButtonPrimary'
-import notFound from '../assets/notFound.jpg'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ButtonPrimary from "../components/ButtonPrimary";
+import notFound from "../assets/notFound.jpg";
 
 export const Missing = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <article className='bg-white h-screen text-black text-center my-auto' >
-      <img src={notFound} className=' flex items-center mx-auto w-[600px] h-[600px]' alt='/' />
-            <div className="flexGrow">
-                <div className=' mx-auto w-fit items-center' onClick={() => navigate('/')} >
-                    <ButtonPrimary title='Visit Our Homepage'/>
-                </div>
-            </div>
+    <article className="bg-white h-screen text-black text-center my-auto">
+      <img
+        src={notFound}
+        className=" flex items-center mx-auto h-3/4"
+        alt="/"
+      />
+      <div className="flexGrow">
+        <div
+          className=" mx-auto w-fit items-center"
+          onClick={() => navigate("/")}
+        >
+          <ButtonPrimary title="Visit Our Homepage" />
+        </div>
+      </div>
     </article>
-  )
-}
+  );
+};
