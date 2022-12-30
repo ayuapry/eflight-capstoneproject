393e63e88c2798e233b5329480fe797601c27fe7
IrvanRizkyAriansyah <irvanrizky58@gmail.com>
2022-12-30 23:00:47 +0700

    repair skeleton search

--- src/components/SkeletonSearch.jsx
+++ src/components/SkeletonSearch.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Skeleton } from "antd";
 
-const SkeletonSearch = () => {
-  return (
-    <div className="w-full">
-      <div className="flex h-48 shadow-sm bg-gray-100 rounded-xl lg:p-8 p-4 mb-4 lg:mx-20">
+const SkeletonSearch = (props) => {
+  const sklSrc = (i) => {
+    return (
+      <div
+        key={i}
+        className="flex md:h-48 h-32 shadow-sm bg-gray-100 rounded-xl lg:p-8 p-4 mb-4 lg:mx-20"
+      >
         <Skeleton
           avatar
           paragraph={{
-            rows: 3,
+            rows: 2,
           }}
           active
         />
@@ -21,42 +24,15 @@ const SkeletonSearch = () => {
           />
         </div>
       </div>
-      <div className="flex h-48 shadow-sm bg-gray-100 rounded-xl lg:p-8 p-4 mb-4 lg:mx-20">
-        <Skeleton
-          avatar
-          paragraph={{
-            rows: 3,
-          }}
-          active
-        />
-        <div className="hidden lg:block ml-8">
-          <Skeleton.Button
-            active
-            size={"large"}
-            shape={"default"}
-            style={{ width: "10rem" }}
-          />
-        </div>
-      </div>
-      <div className="flex h-48 shadow-sm bg-gray-100 rounded-xl lg:p-8 p-4 mb-4 lg:mx-20">
-        <Skeleton
-          avatar
-          paragraph={{
-            rows: 3,
-          }}
-          active
-        />
-        <div className="hidden lg:block ml-8">
-          <Skeleton.Button
-            active
-            size={"large"}
-            shape={"default"}
-            style={{ width: "10rem" }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  };
+
+  const comp = [];
+
+  for (let index = 0; index < props.length; index++) {
+    comp.push(sklSrc(index));
+  }
+  return <div className="w-full">{comp}</div>;
 };
 
 export default SkeletonSearch;
