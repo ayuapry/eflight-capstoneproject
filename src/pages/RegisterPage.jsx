8ea13d978d1f3470afe9138820618c486480f7ab
Ayuapry <ayuapriyanti0304@gmail.com>
2023-01-04 16:28:34 +0700

    fix bug register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -34,7 +34,7 @@ export default function RegisterPage() {
   console.log(id);
 
   return (
-    <div className="flex flex-col justify-between h-screen max-w-7xl mx-auto ">
+    <div className="flex flex-col justify-between h-screen max-w-[1024] mx-auto ">
       <div className="grid lg:grid-cols-2 md:items-center md:h-[90vh]">
         <div className="hidden ml-20 p-2 lg:w-[100%] lg:block">
           <LazyLoadImage
